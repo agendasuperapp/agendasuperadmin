dab3e75be27e47975cdee218b08b92686fbb4dce
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-11-01 13:34:22 +0000

    -Backup de versões no servidor-Rodapés nas versões Mobile-Melhorias no Layout

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -116,7 +116,7 @@ class FFAppState extends ChangeNotifier {
 
   late FlutterSecureStorage secureStorage;
 
-  String _VarVersaoSistema = '4.1.23';
+  String _VarVersaoSistema = '4.1.24';
   String get VarVersaoSistema => _VarVersaoSistema;
   set VarVersaoSistema(String value) {
     _VarVersaoSistema = value;

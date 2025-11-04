982c8a20be19465a09945a31c60604b9a330b872
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-11-04 02:34:32 +0000

    Versão 4.1.36

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -116,7 +116,7 @@ class FFAppState extends ChangeNotifier {
 
   late FlutterSecureStorage secureStorage;
 
-  String _VarVersaoSistema = '4.1.35';
+  String _VarVersaoSistema = '4.1.36';
   String get VarVersaoSistema => _VarVersaoSistema;
   set VarVersaoSistema(String value) {
     _VarVersaoSistema = value;

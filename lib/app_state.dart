9fc538d0cf1a92bc952465054fdda6182e88ba28
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-11-07 03:58:43 +0000

    teste ícone

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -116,7 +116,7 @@ class FFAppState extends ChangeNotifier {
 
   late FlutterSecureStorage secureStorage;
 
-  String _VarVersaoSistema = '4.1.49';
+  String _VarVersaoSistema = '4.1.50';
   String get VarVersaoSistema => _VarVersaoSistema;
   set VarVersaoSistema(String value) {
     _VarVersaoSistema = value;
